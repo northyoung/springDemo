afeb966b5b3a2d87609d97345803be8ef74a1fac
northyoung <joker.ssy@gmail.com>
2014-06-22 14:19:26 +0800

    init

---
+++ src/cn/itcast/service/impl/PersonServiceBean.java
@@ -0,0 +1,13 @@
+package cn.itcast.service.impl;
+
+import cn.itcast.service.PersonService;
+
+public class PersonServiceBean implements PersonService {
+	/* (non-Javadoc)
+	 * @see cn.itcast.service.impl.PersonService#save()
+	 */
+	@Override
+	public void save(){
+		System.out.println("±£´æ³É¹¦");		
+	}
+}
---
+++ src/junit/test/SpringTest.java
@@ -0,0 +1,25 @@
+package junit.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.itcast.service.PersonService;
+
+public class SpringTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void instanceSpring() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		PersonService personService = (PersonService)ctx.getBean("personService");
+		personService.save();
+	}
+	
+}
